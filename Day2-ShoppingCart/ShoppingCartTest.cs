bc3e776d1559760f124d7f660537a39c056ff265
曾義格 <tsengeagle@gmail.com>
2016-03-13 20:35:55 +0800

    第二個情境，加入打折邏輯，紅燈

--- Day2-ShoppingCart/ShoppingCartTest.cs
+++ Day2-ShoppingCart/ShoppingCartTest.cs
@@ -7,18 +7,40 @@ namespace Day2_ShoppingCart
     public class ShoppingCartTest
     {
         [TestMethod]
-        public void PromotionTest_GivenBuyVolOneFor_1_ThenHitCheckOut_AmountShouldBe_100()
+        public void PromotionTest_GivenBuyVolOneFor_1_ThenBuyVolTwoFor_1_ThenHitCheckOut_TotalAmountShouldBe190()
         {
             //arrange
-            Book book = new Book();
-            book.Name = "哈利波特第一集";
-            //漏了價格
-            book.Price = 100;
+            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
+            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
 
             ShoppingCart shoppingCart = new ShoppingCart();
-            shoppingCart.AddIn(book);
+            shoppingCart.AddIn(poterVol1);
+            shoppingCart.AddIn(poterVol2);
 
-            double expectedAmount = book.Price;
+            double expectedAmount = (poterVol1.Price + poterVol2.Price) * 0.95;
+
+            //act
+            shoppingCart.Checkout();
+
+            double actualAmount = shoppingCart.TotalAmount;
+
+            //assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
+        public void PromotionTest_GivenBuyVolOneFor_1_ThenHitCheckOut_TotalAmountShouldBe_100()
+        {
+            //arrange
+            Book poterVol1 = new Book();
+            poterVol1.Name = "哈利波特第一集";
+            //漏了價格
+            poterVol1.Price = 100;
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.AddIn(poterVol1);
+
+            double expectedAmount = poterVol1.Price;
 
             //act
             shoppingCart.Checkout();
