d570f0dc475cc6b5375acf0ccd6327d020a11900
曾義格 <tsengeagle@gmail.com>
2016-03-13 23:11:51 +0800

    案例7，3本9折＆2本95折

--- Day2-ShoppingCart/ShoppingCartTest.cs
+++ Day2-ShoppingCart/ShoppingCartTest.cs
@@ -6,6 +6,42 @@ namespace Day2_ShoppingCart
     public class ShoppingCartTest
     {
         [TestMethod]
+        public void PromotionTest_GivenBuyVol_1_For_1_ThenBuyVol_2And3_EachFor_2_ThenHitCheckout_TotalAmountShouldBe460()
+        {
+            //arrange
+            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
+            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
+            Book anotherPoterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
+            Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
+            Book anotherPoterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.AddIn(poterVol1);
+            shoppingCart.AddIn(poterVol2);
+            shoppingCart.AddIn(poterVol3);
+            shoppingCart.AddIn(anotherPoterVol2);
+            shoppingCart.AddIn(anotherPoterVol3);
+
+            double expectedAmount =
+                (
+                    (poterVol1.Price + poterVol2.Price + poterVol3.Price) * 0.90
+                )
+                +
+                (
+                    (anotherPoterVol2.Price + anotherPoterVol3.Price) * 0.95
+                )
+                ;
+
+            //act
+            shoppingCart.Checkout();
+
+            double actualAmount = shoppingCart.TotalAmount;
+
+            //assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
         public void PromotionTest_GivenBuyVol_1And2_EachFor_1_ThenBuyVol_3_For2_ThenHitCheckout_TotalAmountShouldBe_370()
         {
             //arrange
@@ -26,7 +62,7 @@ namespace Day2_ShoppingCart
                 )
                 +
                 (
-                    poterVol3.Price
+                    addPoterVol3.Price
                 )
                 ;
 
