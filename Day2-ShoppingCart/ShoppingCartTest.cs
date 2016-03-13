0d6b58373764ba02833548744d647081da2dd9cb
曾義格 <tsengeagle@gmail.com>
2016-03-13 23:21:49 +0800

    買12355

--- Day2-ShoppingCart/ShoppingCartTest.cs
+++ Day2-ShoppingCart/ShoppingCartTest.cs
@@ -76,6 +76,42 @@ namespace Day2_ShoppingCart
         }
 
         [TestMethod]
+        public void PromotionTest_GivenBuyVol_1To3_EachFor_1_ThenBuyVol_5_For_2_ThenHitCheckout_TotalAmountShouldBe()
+        {
+            //arrange
+            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
+            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
+            Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
+            Book poterVol5 = new Book() { Name = "哈利波特第五集", Price = 100 };
+            Book anotherPoterVol5 = new Book() { Name = "哈利波特第五集", Price = 100 };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.AddIn(poterVol1);
+            shoppingCart.AddIn(poterVol2);
+            shoppingCart.AddIn(poterVol3);
+            shoppingCart.AddIn(poterVol5);
+            shoppingCart.AddIn(anotherPoterVol5);
+
+            double expectedAmount =
+                (
+                    (poterVol1.Price + poterVol2.Price + poterVol3.Price + poterVol5.Price) * 0.80
+                )
+                +
+                (
+                    (anotherPoterVol5.Price)
+                )
+                ;
+
+            //act
+            shoppingCart.Checkout();
+
+            double actualAmount = shoppingCart.TotalAmount;
+
+            //assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
         public void PromotionTest_GivenBuyVol_1To4_EachFor_1_ThenHitCheckout_TotalAmountShouldBe_320()
         {
             //arrange
