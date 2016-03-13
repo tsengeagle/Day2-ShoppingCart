4eef0062866bdbbe3053da0403714a1d204e9c5d
曾義格 <tsengeagle@gmail.com>
2016-03-13 23:42:55 +0800

    檢查書籍及計算金額，獨立成兩個method

---
+++ Day2-ShoppingCart/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Day2_ShoppingCart
+{
+    internal class Order
+    {
+        public Order()
+        {
+            Items = new List<Book>();
+        }
+
+        public List<Book> Items { get; internal set; }
+    }
+}
--- Day2-ShoppingCart/ShoppingCart.cs
+++ Day2-ShoppingCart/ShoppingCart.cs
@@ -6,8 +6,8 @@ namespace Day2_ShoppingCart
 {
     internal class ShoppingCart
     {
-        private List<Book> _cartItems = new List<Book>();
-        private List<Book> _readyForCheckout = new List<Book>();
+        private List<Order> orders = new List<Order>();
+        private List<Book> products = new List<Book>();
 
         public ShoppingCart()
         {
@@ -17,76 +17,83 @@ namespace Day2_ShoppingCart
 
         internal void AddIn(Book book)
         {
-            _cartItems.Add(book);
+            products.Add(book);
         }
 
         internal void Checkout()
         {
-            //書籍按書名排序
-            _cartItems.OrderBy(o => o.Name);
-
-            //第一本開始
-            foreach (var firstCheck in _cartItems)
-            {
-                if (!firstCheck.Checked)
-                {
-                    //先拿到一邊，註記結帳
-                    _readyForCheckout.Add(firstCheck);
-                    firstCheck.Checked = true;
-
-                    //從還沒結帳，且與剛剛那本書不同書名的書堆中，依照書名群組
-                    var itemsGroup = _cartItems.Where(w => (!w.Checked) && (w.Name != firstCheck.Name)).GroupBy(g => g.Name);
-                    //每一組
-                    foreach (var items in itemsGroup)
-                    {
-                        //從還未結帳的書堆中抓第一筆，放進待結帳區，且註記結帳
-                        Book checkedBook = _cartItems.Where(w => (!w.Checked) && (w.Name == items.Key)).First();
-                        _readyForCheckout.Add(checkedBook);
-                        checkedBook.Checked = true;
-                    }
-                    CalculateTotalAmount();
-                }
-            }
+            CheckoutAllProducts();
+            CalculateTotalAmount();
         }
 
         private void CalculateTotalAmount()
         {
-            //放旁邊的每一本
-            foreach (var item in _readyForCheckout)
+            foreach (var orderForCheckout in orders)
             {
-                //看總共有幾本
-                switch (_readyForCheckout.Count)
+                //訂單中的每一本
+                foreach (var item in orderForCheckout.Items)
                 {
-                    //只有一本，原價
-                    case 1:
-                        TotalAmount += item.Price;
-                        break;
+                    //確認折扣
+                    switch (orderForCheckout.Items.Count)
+                    {
+                        //只有一本，原價
+                        case 1:
+                            TotalAmount += item.Price;
+                            break;
 
-                    //兩本，95折
-                    case 2:
-                        TotalAmount += item.Price * 0.95;
-                        break;
+                        //兩本，95折
+                        case 2:
+                            TotalAmount += item.Price * 0.95;
+                            break;
 
-                    //兩本，95折
-                    case 3:
-                        TotalAmount += item.Price * 0.90;
-                        break;
+                        //兩本，95折
+                        case 3:
+                            TotalAmount += item.Price * 0.90;
+                            break;
 
-                    //兩本，95折
-                    case 4:
-                        TotalAmount += item.Price * 0.80;
-                        break;
+                        //兩本，95折
+                        case 4:
+                            TotalAmount += item.Price * 0.80;
+                            break;
 
-                    //兩本，95折
-                    case 5:
-                        TotalAmount += item.Price * 0.75;
-                        break;
+                        //兩本，95折
+                        case 5:
+                            TotalAmount += item.Price * 0.75;
+                            break;
 
-                    default:
-                        break;
+                        default:
+                            break;
+                    }
                 }
             }
-            _readyForCheckout.Clear();
+        }
+
+        private void CheckoutAllProducts()
+        {
+            //第一本開始
+            foreach (var product in products)
+            {
+                if (!product.Checked)
+                {
+                    //用訂單來整理要結帳的商品
+                    Order checkoutOrder = new Order();
+                    checkoutOrder.Items.Add(product);
+                    product.Checked = true;
+
+                    //從還沒結帳，且與剛剛那本書不同書名的書堆中，依照書名群組
+                    var itemsGroup = products.Where(w => (!w.Checked) && (w.Name != product.Name)).GroupBy(g => g.Name);
+                    //每一組
+                    foreach (var items in itemsGroup)
+                    {
+                        //從還未結帳的書堆中抓第一筆，放進訂單，且註記結帳
+                        Book checkedBook = products.Where(w => (!w.Checked) && (w.Name == items.Key)).First();
+                        checkoutOrder.Items.Add(checkedBook);
+                        checkedBook.Checked = true;
+                    }
+
+                    orders.Add(checkoutOrder);
+                }
+            }
         }
     }
 }
