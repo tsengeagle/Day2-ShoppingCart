9ba5f97bddc84e5a417f0b3b32b33c219cf54a57
曾義格 <tsengeagle@gmail.com>
2016-03-13 22:32:48 +0800

    第三個case，3本不一樣的書打8折

--- Day2-ShoppingCart/ShoppingCartTest.cs
+++ Day2-ShoppingCart/ShoppingCartTest.cs
@@ -50,5 +50,29 @@ namespace Day2_ShoppingCart
             //assert
             Assert.AreEqual(expectedAmount, actualAmount);
         }
+
+        [TestMethod]
+        public void PromotionTest_GivenBuyVolOneToVolThreeEachFor_1_ThenHitCheckout_TotalAmountShouldBe_270()
+        {
+            //arrange
+            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
+            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
+            Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.AddIn(poterVol1);
+            shoppingCart.AddIn(poterVol2);
+            shoppingCart.AddIn(poterVol3);
+
+            double expectedAmount = (poterVol1.Price + poterVol2.Price + poterVol3.Price) * 0.90;
+
+            //act
+            shoppingCart.Checkout();
+
+            double actualAmount = shoppingCart.TotalAmount;
+
+            //assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
     }
 }
