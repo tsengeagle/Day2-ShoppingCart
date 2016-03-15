99039afc8e3c65cef32c841c0ed93c1dec6ab343
曾義格 <tsengeagle@gmail.com>
2016-03-15 19:56:16 +0800

    用案例名稱說明情境及預期金額，測試案例裡面不該有商業邏輯

--- Day2-ShoppingCart/ShoppingCartTest.cs
+++ Day2-ShoppingCart/ShoppingCartTest.cs
@@ -6,7 +6,54 @@ namespace Day2_ShoppingCart
     public class ShoppingCartTest
     {
         [TestMethod]
-        public void PromotionTest_GivenBuyVol_1_For_1_ThenBuyVol_2And3_EachFor_2_ThenHitCheckout_TotalAmountShouldBe460()
+        public void PromotionTest_單買1本_按下結帳_原價_預期金額100()
+        {
+            //arrange
+            Book poterVol1 = new Book();
+            poterVol1.Name = "哈利波特第一集";
+            //漏了價格
+            poterVol1.Price = 100;
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.AddIn(poterVol1);
+
+            double expectedAmount = 100;
+
+            //act
+            shoppingCart.Checkout();
+
+            double actualAmount = shoppingCart.TotalAmount;
+
+            //assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
+        public void PromotionTest_買了第一集到第三集各1本_按下結帳_符合3本9折_預期金額270()
+        {
+            //arrange
+            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
+            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
+            Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.AddIn(poterVol1);
+            shoppingCart.AddIn(poterVol2);
+            shoppingCart.AddIn(poterVol3);
+
+            double expectedAmount = 270;
+
+            //act
+            shoppingCart.Checkout();
+
+            double actualAmount = shoppingCart.TotalAmount;
+
+            //assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
+        public void PromotionTest_買第一集1本_第二集2本_第三集2本_按下結帳_符合3本9折與2本95折各1套_預期金額460()
         {
             //arrange
             Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
@@ -22,15 +69,7 @@ namespace Day2_ShoppingCart
             shoppingCart.AddIn(anotherPoterVol2);
             shoppingCart.AddIn(anotherPoterVol3);
 
-            double expectedAmount =
-                (
-                    (poterVol1.Price + poterVol2.Price + poterVol3.Price) * 0.90
-                )
-                +
-                (
-                    (anotherPoterVol2.Price + anotherPoterVol3.Price) * 0.95
-                )
-                ;
+            double expectedAmount = 460;
 
             //act
             shoppingCart.Checkout();
@@ -42,29 +81,23 @@ namespace Day2_ShoppingCart
         }
 
         [TestMethod]
-        public void PromotionTest_GivenBuyVol_1And2_EachFor_1_ThenBuyVol_3_For2_ThenHitCheckout_TotalAmountShouldBe_370()
+        public void PromotionTest_買第一集到第五集全套_按下結帳_符合5本75折_預期金額375()
         {
             //arrange
             Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
             Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
             Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
-            Book addPoterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
+            Book poterVol4 = new Book() { Name = "哈利波特第四集", Price = 100 };
+            Book poterVol5 = new Book() { Name = "哈利波特第五集", Price = 100 };
 
             ShoppingCart shoppingCart = new ShoppingCart();
             shoppingCart.AddIn(poterVol1);
             shoppingCart.AddIn(poterVol2);
             shoppingCart.AddIn(poterVol3);
-            shoppingCart.AddIn(addPoterVol3);
+            shoppingCart.AddIn(poterVol4);
+            shoppingCart.AddIn(poterVol5);
 
-            double expectedAmount =
-                (
-                    (poterVol1.Price + poterVol2.Price + poterVol3.Price) * 0.90
-                )
-                +
-                (
-                    addPoterVol3.Price
-                )
-                ;
+            double expectedAmount = 375;
 
             //act
             shoppingCart.Checkout();
@@ -76,7 +109,55 @@ namespace Day2_ShoppingCart
         }
 
         [TestMethod]
-        public void PromotionTest_GivenBuyVol_1To3_EachFor_1_ThenBuyVol_5_For_2_ThenHitCheckout_TotalAmountShouldBe()
+        public void PromotionTest_買第一集到第四集各1本_按下結帳_符合4本8折_預期金額320()
+        {
+            //arrange
+            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
+            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
+            Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
+            Book poterVol4 = new Book() { Name = "哈利波特第四集", Price = 100 };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.AddIn(poterVol1);
+            shoppingCart.AddIn(poterVol2);
+            shoppingCart.AddIn(poterVol3);
+            shoppingCart.AddIn(poterVol4);
+
+            double expectedAmount = 320;
+
+            //act
+            shoppingCart.Checkout();
+
+            double actualAmount = shoppingCart.TotalAmount;
+
+            //assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
+        public void PromotionTest_買第一集和第二集各1本_按下結帳_符合2本95折_預期金額190()
+        {
+            //arrange
+            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
+            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.AddIn(poterVol1);
+            shoppingCart.AddIn(poterVol2);
+
+            double expectedAmount = 190;
+
+            //act
+            shoppingCart.Checkout();
+
+            double actualAmount = shoppingCart.TotalAmount;
+
+            //assert
+            Assert.AreEqual(expectedAmount, actualAmount);
+        }
+
+        [TestMethod]
+        public void PromotionTest_買第一集第二集第三集各1本_再買第5集2本_按下結帳_符合4本8折與1本原價_預期金額420()
         {
             //arrange
             Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
@@ -92,15 +173,7 @@ namespace Day2_ShoppingCart
             shoppingCart.AddIn(poterVol5);
             shoppingCart.AddIn(anotherPoterVol5);
 
-            double expectedAmount =
-                (
-                    (poterVol1.Price + poterVol2.Price + poterVol3.Price + poterVol5.Price) * 0.80
-                )
-                +
-                (
-                    (anotherPoterVol5.Price)
-                )
-                ;
+            double expectedAmount = 420;
 
             //act
             shoppingCart.Checkout();
@@ -112,125 +185,21 @@ namespace Day2_ShoppingCart
         }
 
         [TestMethod]
-        public void PromotionTest_GivenBuyVol_1To4_EachFor_1_ThenHitCheckout_TotalAmountShouldBe_320()
+        public void PromotionTest_買第一集與第二集各1本_再買第三集2本_按下結帳_符合3本9折及1本原價_預期金額370()
         {
             //arrange
             Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
             Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
             Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
-            Book poterVol4 = new Book() { Name = "哈利波特第四集", Price = 100 };
+            Book addPoterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
 
             ShoppingCart shoppingCart = new ShoppingCart();
             shoppingCart.AddIn(poterVol1);
             shoppingCart.AddIn(poterVol2);
             shoppingCart.AddIn(poterVol3);
-            shoppingCart.AddIn(poterVol4);
+            shoppingCart.AddIn(addPoterVol3);
 
-            double expectedAmount = (poterVol1.Price + poterVol2.Price + poterVol3.Price + poterVol4.Price) * 0.80;
-
-            //act
-            shoppingCart.Checkout();
-
-            double actualAmount = shoppingCart.TotalAmount;
-
-            //assert
-            Assert.AreEqual(expectedAmount, actualAmount);
-        }
-
-        [TestMethod]
-        public void PromotionTest_GivenBuyVol_1To5_EachFor_1_ThenHitCheckout_TotalAmountShouldBe_375()
-        {
-            //arrange
-            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
-            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
-            Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
-            Book poterVol4 = new Book() { Name = "哈利波特第四集", Price = 100 };
-            Book poterVol5 = new Book() { Name = "哈利波特第五集", Price = 100 };
-
-            ShoppingCart shoppingCart = new ShoppingCart();
-            shoppingCart.AddIn(poterVol1);
-            shoppingCart.AddIn(poterVol2);
-            shoppingCart.AddIn(poterVol3);
-            shoppingCart.AddIn(poterVol4);
-            shoppingCart.AddIn(poterVol5);
-
-            double expectedAmount =
-                (
-                    poterVol1.Price +
-                    poterVol2.Price +
-                    poterVol3.Price +
-                    poterVol4.Price +
-                    poterVol5.Price
-                ) * 0.75;
-
-            //act
-            shoppingCart.Checkout();
-
-            double actualAmount = shoppingCart.TotalAmount;
-
-            //assert
-            Assert.AreEqual(expectedAmount, actualAmount);
-        }
-
-        [TestMethod]
-        public void PromotionTest_GivenBuyVolOneFor_1_ThenBuyVolTwoFor_1_ThenHitCheckOut_TotalAmountShouldBe190()
-        {
-            //arrange
-            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
-            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
-
-            ShoppingCart shoppingCart = new ShoppingCart();
-            shoppingCart.AddIn(poterVol1);
-            shoppingCart.AddIn(poterVol2);
-
-            double expectedAmount = (poterVol1.Price + poterVol2.Price) * 0.95;
-
-            //act
-            shoppingCart.Checkout();
-
-            double actualAmount = shoppingCart.TotalAmount;
-
-            //assert
-            Assert.AreEqual(expectedAmount, actualAmount);
-        }
-
-        [TestMethod]
-        public void PromotionTest_GivenBuyVolOneFor_1_ThenHitCheckOut_TotalAmountShouldBe_100()
-        {
-            //arrange
-            Book poterVol1 = new Book();
-            poterVol1.Name = "哈利波特第一集";
-            //漏了價格
-            poterVol1.Price = 100;
-
-            ShoppingCart shoppingCart = new ShoppingCart();
-            shoppingCart.AddIn(poterVol1);
-
-            double expectedAmount = poterVol1.Price;
-
-            //act
-            shoppingCart.Checkout();
-
-            double actualAmount = shoppingCart.TotalAmount;
-
-            //assert
-            Assert.AreEqual(expectedAmount, actualAmount);
-        }
-
-        [TestMethod]
-        public void PromotionTest_GivenBuyVolOneToVolThreeEachFor_1_ThenHitCheckout_TotalAmountShouldBe_270()
-        {
-            //arrange
-            Book poterVol1 = new Book() { Name = "哈利波特第一集", Price = 100 };
-            Book poterVol2 = new Book() { Name = "哈利波特第二集", Price = 100 };
-            Book poterVol3 = new Book() { Name = "哈利波特第三集", Price = 100 };
-
-            ShoppingCart shoppingCart = new ShoppingCart();
-            shoppingCart.AddIn(poterVol1);
-            shoppingCart.AddIn(poterVol2);
-            shoppingCart.AddIn(poterVol3);
-
-            double expectedAmount = (poterVol1.Price + poterVol2.Price + poterVol3.Price) * 0.90;
+            double expectedAmount = 370;
 
             //act
             shoppingCart.Checkout();
